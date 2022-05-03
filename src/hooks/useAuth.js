d587400813d3c7e104fea0d8f3fb769a466da990
Huan Tran <tranhuan.dev@gmail.com>
2022-05-03 16:56:08 +0700

    fix loading profile after edit

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,6 +1,6 @@
 import { onAuthStateChanged } from 'firebase/auth';
 import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { checkLogged } from '../components/Login/loginSlice';
 import { auth } from '../firebase/config';
 
@@ -10,9 +10,11 @@ const useAuth = () => {
   const [isLoading, setIsLoading] = useState(true);
   const currentUserAuth = auth.currentUser;
 
+  const { isLogged } = useSelector((state) => state.login);
+
   useEffect(() => {
     const unsubscribed = onAuthStateChanged(auth, (user) => {
-      if (user) {
+      if (user && !isLogged) {
         dispatch(checkLogged(true));
         setIsLoading(false);
         return;
@@ -21,7 +23,7 @@ const useAuth = () => {
     });
 
     return unsubscribed;
-  }, [dispatch]);
+  }, [dispatch, isLogged]);
 
   return { isLoading, currentUserAuth };
 };
